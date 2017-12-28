5b89e8e697c53fdc969f967cee6e772bee0d9bf3
Joel Guizar <joelguizar@gmail.com>
2017-12-27 22:28:09 -0800

    beforeEach initalized

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -5,15 +5,24 @@ import {renderComponent, expect} from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
+  //function that is run before any it statement
+  let component;
+  
+  beforeEach(()=>{
+    component = renderComponent(CommentBox);
+  })
+
+  it('has the correct class', ()=>{
+    expect(component).to.have.class('comment-box');
+  });
+
   it('has a text area', ()=>{
     //renderComponent is returning jquery object wrapping the commentbox
     //so we can use jquery test helpers with this now.
-    const component = renderComponent(CommentBox);
     expect(component.find('textarea')).to.exist;
   });
 
   it('it has a button', ()=> {
-    const component = renderComponent(CommentBox);
     expect(component.find('button')).to.exist;
   });
 });
