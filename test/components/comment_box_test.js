50f14584d4ac5c0114eb04e9957452bdadde20b8
Joel Guizar <joelguizar@gmail.com>
2017-12-27 22:11:12 -0800

    commentbox test

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -6,11 +6,14 @@ import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
   it('has a text area', ()=>{
+    //renderComponent is returning jquery object wrapping the commentbox
+    //so we can use jquery test helpers with this now.
     const component = renderComponent(CommentBox);
     expect(component.find('textarea')).to.exist;
   });
 
   it('it has a button', ()=> {
-
+    const component = renderComponent(CommentBox);
+    expect(component.find('button')).to.exist;
   });
 });
