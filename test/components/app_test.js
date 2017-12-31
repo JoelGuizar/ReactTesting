287d583e476b7307411e01e6be7393228d26ac64
Joel Guizar <joelguizar@gmail.com>
2017-12-30 17:33:07 -0800

    fixed initial app test

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -7,11 +7,11 @@ describe('App', () => {
   let component;
 
   beforeEach(()=> {
-    component = renderComponent(App)
+    component = renderComponent(App);
   });
-  
+
   it('shows a comment box', () => {
     //find is a jQuery test method that accepts a CSS selector
-    expect(component.find('.comment-box').to.exist)
+    expect(component.find('.comment-box')).to.exist
   })
 });
